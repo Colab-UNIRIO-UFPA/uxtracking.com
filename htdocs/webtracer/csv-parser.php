8d523ed9c6e5dd1fa927a17be6391f83f3cb360d
ViniNeves6 <91764267+ViniNeves6@users.noreply.github.com>
2022-08-30 16:13:24 -0300

    Delete htdocs/webtracer directory

--- htdocs/webtracer/csv-parser.php
+++
@@ -1,7 +0,0 @@
-<?php
-	if ($_SERVER["REQUEST_METHOD"] == "POST")
-	{
-        $data = json_decode($_POST['data']);
-        file_put_contents('output.csv', $data.PHP_EOL , FILE_APPEND | LOCK_EX);
-    }
-?>

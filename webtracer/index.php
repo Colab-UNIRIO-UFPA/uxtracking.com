0f6ba74aa2ea3acf0d69ef1a2e14225e656f9eba
Flávio Moura <62734053+Horusprg@users.noreply.github.com>
2023-01-23 12:52:41 -0300

    Update index.php

--- webtracer/index.php
+++ webtracer/index.php
@@ -1,3 +1,63 @@
+<!doctype html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>UX-Tracking: Web and Multimodal Tool for User Experience Evaluation</title>
+    <link rel="stylesheet" href="css/index.css" />
+</head>
+
+<body>
+    <div id="particles-js"></div>
+    <div id="logo">
+        <img src="logo.png">
+    </div>
+    <div id="div-botoes">
+        <ul class="menu">
+            <form method='post' action=''>
+                <input class="botoes-menu" type='submit' name='downloadResearch' value='Download Research' />&nbsp;
+                <input class="botoes-menu" type='submit' name='downloadExtension' value='Extensão' />&nbsp;
+                <input class="botoes-menu" type='submit' name='downloadTools' value='Ferramentas de Pós-processamento' />&nbsp;
+                <a href="https://github.com/Colab-UNIRIO-UFPA/UX-Tracking#readme" onclick="link(this)">
+                    <button class="botoes-menu">Manual de Uso</button>
+                </a>
+                <a href="https://github.com/Colab-UNIRIO-UFPA/UX-Tracking" onclick="link(this)">
+                    <button class="botoes-menu">Repositório</button>
+                </a>
+            </form>
+        </ul>
+    </div>
+    &nbsp;
+    <div class="texto">
+        <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            A UX-Tracking é uma ferramenta que monitora, de forma automatizada, a
+            experiência do usuário (neste caso, desenvolvedor) no ambiente Web e
+            gera artefatos visuais e indicadores para documentá-la, permitindo a
+            realização de análises e diagnósticos sobre a percepção do desenvolvedor
+            de quão fácil é navegar em um portal e se há algum problema que gere uma
+            experiência ruim. A ferramenta é composta por três módulos principais: o
+            módulo cliente, o módulo de armazenamento e o módulo de visualizacão de
+            dados, que são os responsÃ¡veis por capturar as interações dos desenvolvedores,
+            armazenar e disponibilizar os resultados da interação, respectivamente (SOUZA et al, 2020).</p>
+        <p>Possíveis variáveis que a ferramenta pode coletar:</p>
+        <ul>
+            <li>Distância total do mouse</li>
+            <li>Atraso na decisão de clicar</li>
+            <li>Número total de cliques</li>
+            <li>Tempo de fixação do olhar</li>
+        </ul>
+    </div>
+    <div class='container'>
+    </div>
+</body>
+    <script type="text/javascript" src="js/particles.js"></script>
+	<script type="text/javascript" src="js/app.js"></script>
+	<script type="text/javascript" src="js/opener.js"></script>
+    
+</html>
+
 <?php
 // Create ZIP file
 if (isset($_POST['downloadResearch'])) {
@@ -114,62 +174,3 @@ function recursiveRemove($dir)
 }
 
 ?>
-<!doctype html>
-<html>
-
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>UX-Tracking: Web and Multimodal Tool for User Experience Evaluation</title>
-    <link rel="stylesheet" href="css/index.css" />
-</head>
-
-<body>
-    <div id="particles-js"></div>
-    <div id="logo">
-        <img src="logo.png">
-    </div>
-    <div id="div-botoes">
-        <ul class="menu">
-            <form method='post' action=''>
-                <input class="botoes-menu" type='submit' name='downloadResearch' value='Download Research' />&nbsp;
-                <input class="botoes-menu" type='submit' name='downloadExtension' value='Extensão' />&nbsp;
-                <input class="botoes-menu" type='submit' name='downloadTools' value='Ferramentas de Pós-processamento' />&nbsp;
-                <a href="https://github.com/Colab-UNIRIO-UFPA/UX-Tracking#readme" onclick="link(this)">
-                    <button class="botoes-menu">Manual de Uso</button>
-                </a>
-                <a href="https://github.com/Colab-UNIRIO-UFPA/UX-Tracking" onclick="link(this)">
-                    <button class="botoes-menu">Repositório</button>
-                </a>
-            </form>
-        </ul>
-    </div>
-    &nbsp;
-    <div class="texto">
-        <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            A UX-Tracking é uma ferramenta que monitora, de forma automatizada, a
-            experiência do usuário (neste caso, desenvolvedor) no ambiente Web e
-            gera artefatos visuais e indicadores para documentá-la, permitindo a
-            realização de análises e diagnósticos sobre a percepção do desenvolvedor
-            de quão fácil é navegar em um portal e se há algum problema que gere uma
-            experiência ruim. A ferramenta é composta por três módulos principais: o
-            módulo cliente, o módulo de armazenamento e o módulo de visualizacão de
-            dados, que são os responsÃ¡veis por capturar as interações dos desenvolvedores,
-            armazenar e disponibilizar os resultados da interação, respectivamente (SOUZA et al, 2020).</p>
-        <p>Possíveis variáveis que a ferramenta pode coletar:</p>
-        <ul>
-            <li>Distância total do mouse</li>
-            <li>Atraso na decisão de clicar</li>
-            <li>Número total de cliques</li>
-            <li>Tempo de fixação do olhar</li>
-        </ul>
-    </div>
-    <div class='container'>
-    </div>
-</body>
-    <script type="text/javascript" src="js/particles.js"></script>
-	<script type="text/javascript" src="js/app.js"></script>
-	<script type="text/javascript" src="js/opener.js"></script>
-    
-</html>

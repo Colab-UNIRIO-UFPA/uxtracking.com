8d90ae17bf273f7b040d281bf2f46e5b0be74664
Flávio Moura <62734053+Horusprg@users.noreply.github.com>
2023-01-27 13:24:06 -0300

    Update index.php

--- webtracer/index.php
+++ webtracer/index.php
@@ -117,7 +117,6 @@ if (isset($_POST['downloadTools'])) {
     if (file_exists($filename)) {
         header('Content-Type: application/zip');
         header('Content-Disposition: attachment; filename="' . basename($filename) . '"');
-        header('Content-Length: ' . filesize($filename));
 
         flush();
         readfile($filename);

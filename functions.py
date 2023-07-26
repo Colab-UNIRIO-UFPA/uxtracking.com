06faed7fab56e3d8149196db750dd37acf9659ca
Flávio Moura <62734053+Horusprg@users.noreply.github.com>
2023-07-26 04:46:25 -0300

    v0.7.2
    - Ajustes nas rotas da aplicação web
    - Antigo json de usuários descartado
    - Update nos requisítos
    - Funções não mais usadas removidas

--- functions.py
+++ functions.py
@@ -71,11 +71,6 @@ def model_meanshift (dados,n_qualite,samples):
   print(f"Número estimado de clusters: {n_clusters_}")
   return
 
-def generate_user_id(name, email):
-    string_to_hash = f"{name}{email}"
-    hashed_string = hashlib.md5(string_to_hash.encode())
-    return hashed_string.hexdigest()
-
 def list_dates(dir):
     dates = []
     folders = os.listdir(dir)
@@ -91,14 +86,6 @@ def list_dates(dir):
                                 f'{time[0]}-{time[1]}'],
                                 dates))
     return dates
-def auth(name, email):
-    with open("users.json", "r") as arquivo:
-        users = json.load(arquivo)
-    if any(user["name"] == name for user in users):
-        return [True, "Usuário já existe."]
-    if any(user["email"] == email for user in users):
-        return [True, "Email já cadastrado."]
-    return [False, None]
 
 def id_generator():
     chars=string.ascii_uppercase + string.digits
